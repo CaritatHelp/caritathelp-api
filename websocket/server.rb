ba1c11d4eeba6837eceae0c2e1efdc115f93c391
Rob <robin.vasseur1@gmail.com>
2016-06-25 15:30:52 +0200

    fix websocket to avoid interruption

--- websocket/server.rb
+++ websocket/server.rb
@@ -3,7 +3,7 @@ require 'pg'
 require 'daemons'
 require 'json'
 
-# Daemons.daemonize
+Daemons.daemonize
 
 sEND_MSG_CARITATHELP = "MCJORR982-ZJHFfe541wxcvbpoizin23231598765185479531dezrjnjhjkae94g41zz4r8g416g5t4eg6io4iop4lkyy4zzefdf"
 nOTIF_CARITATHELP = "JDEje9578efr9zeUPAMD65"
@@ -17,39 +17,48 @@ nOTIF_CARITATHELP = "JDEje9578efr9zeUPAMD65"
 
 EventMachine.run do
 
-  EventMachine::WebSocket.start(host: "0.0.0.0", port: 8080, debug: true) do |ws|
+  EventMachine::WebSocket.start(host: "172.31.31.97", port: 8080, debug: true) do |ws|
     ws.onopen do      
       begin
         current_user_token = nil
         @channel = EM::Channel.new
         
-        sid = @channel.subscribe { |msg| ws.send(msg) }        
+        sid = @channel.subscribe { |msg|
+          begin
+            ws.send(msg)
+          rescue => e
+            p "On subscribe: " + e.message.to_s
+          end
+        }
 
         ws.onmessage { |msg|
-
-          json_msg = JSON.parse(msg)
-
-          if json_msg['token'].eql?('token')
-            if current_user_token.eql?(nil)
-              current_user_token = json_msg['token_user']
-              @channels_list[current_user_token] = @channel
-            end
+          begin
+            json_msg = JSON.parse(msg)
             
-          elsif @tokens_types.key?(json_msg['token'])
-            json_msg['type'] = @tokens_types[json_msg['token']]
-
-            concerned_volunteers = json_msg['concerned_volunteers']            
-            json_msg.delete('concerned_volunteers')
-            json_msg.delete('token')
-            
-            if concerned_volunteers != nil and concerned_volunteers.count > 0
-              concerned_volunteers.each do |volunteer|
-                if @channels_list[volunteer['token']] != nil
-                  @channels_list[volunteer['token']]
-                    .push(json_msg.to_json)
+            if json_msg['token'].eql?('token')
+              if current_user_token.eql?(nil)
+                current_user_token = json_msg['token_user']
+                @channels_list[current_user_token] = @channel
+              end
+              
+            elsif @tokens_types.key?(json_msg['token'])
+              json_msg['type'] = @tokens_types[json_msg['token']]
+              
+              concerned_volunteers = json_msg['concerned_volunteers']            
+              json_msg.delete('concerned_volunteers')
+              json_msg.delete('token')
+              
+              if concerned_volunteers != nil and concerned_volunteers.count > 0
+                concerned_volunteers.each do |volunteer|
+                  if @channels_list[volunteer['token']] != nil
+                    @channels_list[volunteer['token']]
+                      .push(json_msg.to_json)
+                  end
                 end
               end
             end
+          rescue => e
+            p "On send: " + e.message.to_s
           end
         }
         
@@ -59,7 +68,7 @@ EventMachine.run do
           begin
             @channels_list.delete(current_user_token).unsubscribe(sid)
           rescue => e
-            e.message.to_s
+            p "On close:" + e.message.to_s
           end
         }
       rescue => e
