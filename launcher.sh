68c8ef6269f7fdc6c737d2f4142f916177ecf8b8
Robin Vasseur <robin.vasseur@epitech.eu>
2016-06-26 17:55:24 +0200

    rm old script

--- launcher.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-kill `cat daemons.rb.pid`
-sudo kill `cat tmp/pids/server.pid`
-export NOTIF_CARITATHELP=`cat NOTIF_CARITATHELP`
-export SEND_MSG_CARITATHELP=`cat SEND_MSG_CARITATHELP`
-ruby websocket/server.rb
-rvmsudo -E rails s -b 172.31.31.97 -p 80 -d
