c0ec1d64f458007e9bf0f97bd0a1ed424789f741
Robin Vasseur <robin.vasseur@epitech.eu>
2016-06-28 20:52:12 +0200

    fix issue 50, add news seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,123 +10,123 @@
 # Volunteers
 
 robin = Volunteer.create([firstname: 'Robin', lastname: 'Vasseur',
-                  mail: 'robin@root.com', password: 'root'])
+                  mail: 'robin@root.com', password: 'root']).first
 pierre = Volunteer.create([firstname: 'Pierre', lastname: 'Enjalbert',
-                  mail: 'pierre@root.com', password: 'root'])
+                  mail: 'pierre@root.com', password: 'root']).first
 aude = Volunteer.create([firstname: 'Aude', lastname: 'Sikorav',
-                  mail: 'aude@root.com', password: 'root'])
+                  mail: 'aude@root.com', password: 'root']).first
 jeremy = Volunteer.create([firstname: 'Jeremy', lastname: 'Gros',
-                  mail: 'jeremy@root.com', password: 'root'])
+                  mail: 'jeremy@root.com', password: 'root']).first
 nicolas = Volunteer.create([firstname: 'Nicolas', lastname: 'Temenides',
-                  mail: 'nicolas@root.com', password: 'root'])
+                  mail: 'nicolas@root.com', password: 'root']).first
 jerome = Volunteer.create([firstname: 'Jerome', lastname: 'Caudoux',
-                  mail: 'jerome@root.com', password: 'root'])
+                  mail: 'jerome@root.com', password: 'root']).first
 
 # VFriends
 
-VFriend.create([volunteer_id: robin[0][:id], friend_volunteer_id: pierre[0][:id]])
-VFriend.create([volunteer_id: pierre[0][:id], friend_volunteer_id: robin[0][:id]])
-VFriend.create([volunteer_id: robin[0][:id], friend_volunteer_id: nicolas[0][:id]])
-VFriend.create([volunteer_id: nicolas[0][:id], friend_volunteer_id: robin[0][:id]])
+VFriend.create([volunteer_id: robin[:id], friend_volunteer_id: pierre[:id]])
+VFriend.create([volunteer_id: pierre[:id], friend_volunteer_id: robin[:id]])
+VFriend.create([volunteer_id: robin[:id], friend_volunteer_id: nicolas[:id]])
+VFriend.create([volunteer_id: nicolas[:id], friend_volunteer_id: robin[:id]])
 
 # Assocs
 
 croix_rouge = Assoc.create([name: 'Croix verte', description: 'Croix verte du swag',
                            birthday: '02/12/2015', city: 'Paris',
-                           latitude: 9.99, longitude: 9.99])
+                           latitude: 9.99, longitude: 9.99]).first
 resto  = Assoc.create([name: "Les resto de l'estomac", description: 'Pour se remplir le bide',
                            birthday: '02/12/2015', city: 'Là-bas',
-                           latitude: 9.99, longitude: 9.99])
+                           latitude: 9.99, longitude: 9.99]).first
 
 # AvLinks
 
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: robin[0][:id], rights: 'owner'])
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: nicolas[0][:id], rights: 'admin'])
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: pierre[0][:id], rights: 'admin'])
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: jeremy[0][:id], rights: 'member'])
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: aude[0][:id], rights: 'member'])
-AvLink.create([assoc_id: croix_rouge[0][:id], volunteer_id: jerome[0][:id], rights: 'member'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: robin[:id], rights: 'owner'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: nicolas[:id], rights: 'admin'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: pierre[:id], rights: 'admin'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: jeremy[:id], rights: 'member'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: aude[:id], rights: 'member'])
+AvLink.create([assoc_id: croix_rouge[:id], volunteer_id: jerome[:id], rights: 'member'])
 
-AvLink.create([assoc_id: resto[0][:id], volunteer_id: robin[0][:id], rights: 'member'])
-AvLink.create([assoc_id: resto[0][:id], volunteer_id: pierre[0][:id], rights: 'owner'])
-AvLink.create([assoc_id: resto[0][:id], volunteer_id: aude[0][:id], rights: 'admin'])
+AvLink.create([assoc_id: resto[:id], volunteer_id: robin[:id], rights: 'member'])
+AvLink.create([assoc_id: resto[:id], volunteer_id: pierre[:id], rights: 'owner'])
+AvLink.create([assoc_id: resto[:id], volunteer_id: aude[:id], rights: 'admin'])
 
 # Events
 
 event_one = Event.create([title: 'Soignage de gens', description: 'On va soigner des gens',
                          place: 'Zimbabwe', begin: '21/02/2016', end: '22/02/2016', assoc_id: 1,
-                         assoc_name: croix_rouge[0][:name]])
+                         assoc_name: croix_rouge[:name]]).first
 event_two = Event.create([title: 'Sauvetage du soldat Ryan',
                           description: 'Pour faire plaisir à sa maman',
                          place: 'Normandie', begin: '06/06/1970', end: '07/06/1970', assoc_id: 1,
-                         assoc_name: croix_rouge[0][:name]])
+                         assoc_name: croix_rouge[:name]]).first
 event_three = Event.create([title: 'Donnage de miam miam', description: 'bonap',
                          place: 'Paris', begin: '21/07/2016', end: '22/07/2016', assoc_id: 2,
-                         assoc_name: resto[0][:name]])
+                         assoc_name: resto[:name]]).first
 event_four = Event.create([title: 'Soirée Pizza !', description: "Mais seulement avec de l'ananas",
                          place: 'Italie', begin: '01/05/2016', end: '30/12/2016', assoc_id: 2,
-                         assoc_name: resto[0][:name]])
+                         assoc_name: resto[:name]]).first
 event_five = Event.create([title: 'Buffet à volonté', description: 'Sushi Maki Brochette',
                          place: 'Tokyo-Chine', begin: '29/07/2016', end: '30/07/2016', assoc_id: 2,
-                         assoc_name: resto[0][:name]])
+                         assoc_name: resto[:name]]).first
 
 # EventVolunteers
 
-EventVolunteer.create([event_id: event_one[0][:id], volunteer_id: robin[0][:id], rights: 'host'])
-EventVolunteer.create([event_id: event_one[0][:id], volunteer_id: nicolas[0][:id], rights: 'admin'])
-EventVolunteer.create([event_id: event_one[0][:id], volunteer_id: pierre[0][:id], rights: 'member'])
+EventVolunteer.create([event_id: event_one[:id], volunteer_id: robin[:id], rights: 'host'])
+EventVolunteer.create([event_id: event_one[:id], volunteer_id: nicolas[:id], rights: 'admin'])
+EventVolunteer.create([event_id: event_one[:id], volunteer_id: pierre[:id], rights: 'member'])
 
-EventVolunteer.create([event_id: event_two[0][:id], volunteer_id: nicolas[0][:id], rights: 'host'])
-EventVolunteer.create([event_id: event_two[0][:id], volunteer_id: pierre[0][:id], rights: 'admin'])
-EventVolunteer.create([event_id: event_two[0][:id], volunteer_id: jeremy[0][:id], rights: 'member'])
+EventVolunteer.create([event_id: event_two[:id], volunteer_id: nicolas[:id], rights: 'host'])
+EventVolunteer.create([event_id: event_two[:id], volunteer_id: pierre[:id], rights: 'admin'])
+EventVolunteer.create([event_id: event_two[:id], volunteer_id: jeremy[:id], rights: 'member'])
 
-EventVolunteer.create([event_id: event_three[0][:id], volunteer_id: robin[0][:id], rights: 'host'])
-EventVolunteer.create([event_id: event_four[0][:id], volunteer_id: pierre[0][:id], rights: 'host'])
-EventVolunteer.create([event_id: event_five[0][:id], volunteer_id: aude[0][:id], rights: 'host'])
+EventVolunteer.create([event_id: event_three[:id], volunteer_id: robin[:id], rights: 'host'])
+EventVolunteer.create([event_id: event_four[:id], volunteer_id: pierre[:id], rights: 'host'])
+EventVolunteer.create([event_id: event_five[:id], volunteer_id: aude[:id], rights: 'host'])
 
 # Chatrooms
 
 chatroom_one = Chatroom.create([name: 'Hi everyone', number_volunteers: 6,
-                               number_messages: 6, is_private: false])
+                               number_messages: 6, is_private: false]).first
 chatroom_two = Chatroom.create([name: 'Nico - Rob', number_volunteers: 2,
-                               number_messages: 5, is_private: true])
+                               number_messages: 5, is_private: true]).first
 
 # ChatroomVolunteers
 
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: robin[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: pierre[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: nicolas[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: jeremy[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: aude[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_one[0][:id], volunteer_id: jerome[0][:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: robin[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: pierre[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: nicolas[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: jeremy[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: aude[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_one[:id], volunteer_id: jerome[:id]])
 
-ChatroomVolunteer.create([chatroom_id: chatroom_two[0][:id], volunteer_id: robin[0][:id]])
-ChatroomVolunteer.create([chatroom_id: chatroom_two[0][:id], volunteer_id: nicolas[0][:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_two[:id], volunteer_id: robin[:id]])
+ChatroomVolunteer.create([chatroom_id: chatroom_two[:id], volunteer_id: nicolas[:id]])
 
 # Messages
 
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: robin[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: robin[:id],
                 content: 'Yo mes sous fifres'])
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: nicolas[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: nicolas[:id],
                 content: "J'avoue on est trop soumis à toi, Ô Grand Maître Robin"])
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: pierre[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: pierre[:id],
                 content: "Je dirais même plus: Ô Grand Maître Robin Le Magnifique"])
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: jeremy[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: jeremy[:id],
                 content: "Robin on t'aime tellement"])
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: aude[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: aude[:id],
                 content: "En plus tu es tellement beau et intelligent, c'est ouffff"])
-Message.create([chatroom_id: chatroom_one[0][:id], volunteer_id: jerome[0][:id],
+Message.create([chatroom_id: chatroom_one[:id], volunteer_id: jerome[:id],
                 content: "Moi j'suis nouveau mais j'dois bien avouer qu'ils ont raison"])
 
-Message.create([chatroom_id: chatroom_two[0][:id], volunteer_id: robin[0][:id],
+Message.create([chatroom_id: chatroom_two[:id], volunteer_id: robin[:id],
                 content: "Yoyoyo ça va?"])
-Message.create([chatroom_id: chatroom_two[0][:id], volunteer_id: nicolas[0][:id],
+Message.create([chatroom_id: chatroom_two[:id], volunteer_id: nicolas[:id],
                 content: "Ouais et toi?"])
-Message.create([chatroom_id: chatroom_two[0][:id], volunteer_id: robin[0][:id],
+Message.create([chatroom_id: chatroom_two[:id], volunteer_id: robin[:id],
                 content: "Tranquille, j'ai vu ta mère hier"])
-Message.create([chatroom_id: chatroom_two[0][:id], volunteer_id: nicolas[0][:id],
+Message.create([chatroom_id: chatroom_two[:id], volunteer_id: nicolas[:id],
                 content: "Où ça?"])
-Message.create([chatroom_id: chatroom_two[0][:id], volunteer_id: robin[0][:id],
+Message.create([chatroom_id: chatroom_two[:id], volunteer_id: robin[:id],
                 content: "Dans mon lit, LOL !"])
 
 # Shelters
@@ -143,3 +143,30 @@ Shelter.create([name: 'Auberge de vieillesse', address: 'Rue du swag',
                zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150])
 
 # Notifications
+
+# News
+
+New::Assoc::Status.create([content: "Donnez des sous à la croix verte et vous deviendrez riche", assoc_id: croix_rouge.id])
+New::Assoc::Status.create([content: "Que Dieu vous protège wala", assoc_id: croix_rouge.id])
+New::Assoc::Status.create([content: "Et donnez nous de la thune encore", assoc_id: croix_rouge.id])
+New::Assoc::Status.create([content: "Nous on aime manger", assoc_id: resto.id])
+New::Assoc::Status.create([content: "Et faire des batailles de bouffe", assoc_id: resto.id])
+
+New::Event::Status.create([content: "Qui veut se faire soigner?", event_id: event_one.id])
+New::Event::Status.create([content: "Pourquoi on doit tous risquer sa peau pour un seul soldat?", event_id: event_two.id])
+New::Event::Status.create([content: "PARCE QUE VIVE L'AMERIQUE", event_id: event_two.id])
+New::Event::Status.create([content: "On vous donne à manger mais laissez nous en quand même please", event_id: event_three.id])
+New::Event::Status.create([content: "Va fencu*** ti amo viva italia sisi", event_id: event_four.id])
+New::Event::Status.create([content: "Ah bondou fou foulé dé makihein?", event_id: event_five.id])
+New::Event::Status.create([content: "Sushi maki pas cher", event_id: event_five.id])
+
+New::Volunteer::Status.create([content: "Je m'appelle Robin et j'aime les pommes", volunteer_id: robin.id])
+New::Volunteer::Status.create([content: "Cool non?", volunteer_id: robin.id])
+New::Volunteer::Status.create([content: "Je m'apelle Nicolas et je suis moche", volunteer_id: nicolas.id])
+New::Volunteer::Status.create([content: "League of Legend c'est trop bien", volunteer_id: nicolas.id])
+New::Volunteer::Status.create([content: "J'suis lvl 351 sur overwatch lol", volunteer_id: aude.id])
+New::Volunteer::Status.create([content: "Je suis secretement amoureux de Jeremy", volunteer_id: pierre.id])
+New::Volunteer::Status.create([content: "Je suis secretement amoureux de Pierre", volunteer_id: jeremy.id])
+New::Volunteer::Status.create([content: "J'ai posé une google map sur un site et bim 30 crédits OKLM !", volunteer_id: jerome.id])
+New::Volunteer::Status.create([content: "Moi aussi je t'aime Jeremy", volunteer_id: pierre.id])
+New::Volunteer::Status.create([content: "Haha je rigolais t'es moche Pierre", volunteer_id: jeremy.id])
