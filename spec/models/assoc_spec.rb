555768fd886026a5434f879b380957d18de57c73
Robin <robin.vasseur1@gmail.com>
2016-12-13 11:41:09 +0100

    test indent

--- spec/models/assoc_spec.rb
+++ spec/models/assoc_spec.rb
@@ -32,42 +32,42 @@ RSpec.describe Assoc, type: :model do
 		shelter = FactoryGirl.create(:shelter, assoc_id: assoc.id)
 		owner = FactoryGirl.create(:volunteer)
 		member = FactoryGirl.create(:volunteer)
-    	link_owner = FactoryGirl.create(:av_link, assoc_id: assoc.id, volunteer_id: owner.id, rights: "owner", level: 10)
-    	link_member = FactoryGirl.create(:av_link, assoc_id: assoc.id, volunteer_id: member.id, rights: "member", level: 5)
-    	event = FactoryGirl.create(:event, assoc_id: assoc.id)
+		link_owner = FactoryGirl.create(:av_link, assoc_id: assoc.id, volunteer_id: owner.id, rights: "owner", level: 10)
+		link_member = FactoryGirl.create(:av_link, assoc_id: assoc.id, volunteer_id: member.id, rights: "member", level: 5)
+		event = FactoryGirl.create(:event, assoc_id: assoc.id)
 
-    	new1 = FactoryGirl.create(:news, volunteer_id: owner.id,
-    									group_id: assoc.id,
-    									group_name: assoc.name,
-    									group_type: "Assoc",
-    									group_thumb_path: assoc.thumb_path,
-    									as_group: true,
-    									volunteer_name: owner.fullname,
-    									volunteer_thumb_path: owner.thumb_path)
+		new1 = FactoryGirl.create(:news, volunteer_id: owner.id,
+										group_id: assoc.id,
+										group_name: assoc.name,
+										group_type: "Assoc",
+										group_thumb_path: assoc.thumb_path,
+										as_group: true,
+										volunteer_name: owner.fullname,
+										volunteer_thumb_path: owner.thumb_path)
 
-    	new2 = FactoryGirl.create(:news, volunteer_id: owner.id,
-    									group_id: assoc.id,
-    									group_name: assoc.name,
-    									group_type: "Assoc",
-    									group_thumb_path: assoc.thumb_path,
-    									as_group: true,
-    									volunteer_name: owner.fullname,
-    									volunteer_thumb_path: owner.thumb_path)
+		new2 = FactoryGirl.create(:news, volunteer_id: owner.id,
+										group_id: assoc.id,
+										group_name: assoc.name,
+										group_type: "Assoc",
+										group_thumb_path: assoc.thumb_path,
+										as_group: true,
+										volunteer_name: owner.fullname,
+										volunteer_thumb_path: owner.thumb_path)
 
-    	it "get the association's shelters" do
-    		expect(assoc.shelters.count).to eq(1)
-    	end
+		it "get the association's shelters" do
+			expect(assoc.shelters.count).to eq(1)
+		end
 
-    	it "get the association's volunteers" do
-    		expect(assoc.volunteers.count).to eq(2)
-    	end
+		it "get the association's volunteers" do
+			expect(assoc.volunteers.count).to eq(2)
+		end
 
-    	it "get the association's events" do
-    		expect(assoc.events.count).to eq(1)
-    	end
+		it "get the association's events" do
+			expect(assoc.events.count).to eq(1)
+		end
 
-    	it "get the association's news" do
-    		expect(assoc.news.count).to eq(2)
-    	end
+		it "get the association's news" do
+			expect(assoc.news.count).to eq(2)
+		end
 	end
 end
