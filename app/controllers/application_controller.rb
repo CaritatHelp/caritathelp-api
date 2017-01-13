2797bab1fd4496a6b9c7882de018f47b20269f5d
Robin <robin.vasseur1@gmail.com>
2017-01-13 14:12:31 +0100

    fixed #221

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include CanCan::ControllerAdditions
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
   before_action :set_locale
