cf4c21b064c3f07248d69f0137cb5cb207b2d7de
Robin <robin.vasseur1@gmail.com>
2017-01-15 18:14:37 +0100

    fixed #226

--- db/seeds/all.rb
+++ db/seeds/all.rb
@@ -12,24 +12,24 @@ pierre = Volunteer.create(firstname: 'Pierre',
 													lastname: 'Enjalbert',
 													email: 'pierre@root.com',
 													allowgps: true,
-													latitude: 48.8066706, # kremlin
-													longitude: 2.3654136000000108,
+													latitude: 48.801408, # Versaille
+													longitude: 2.1301220000000285,
 													password: 'root1234')
 
 aude = Volunteer.create(firstname: 'Aude',
 												lastname: 'Sikorav',
 												email: 'aude@root.com',
 												allowgps: true,
-												latitude: 37.7749295, # SF
-												longitude: -122.41941550000001,
+												latitude: 48.8332135, # Paris 14ème
+												longitude: 2.3219197,
 												password: 'root1234')
 
 jeremy = Volunteer.create(firstname: 'Jeremy',
 													lastname: 'Gros',
 													email: 'jeremy@root.com',
 													allowgps: true,
-													latitude: 48.801408, # Versaille
-													longitude: 2.1301220000000285,
+													latitude: 48.8066706, # kremlin
+													longitude: 2.3654136000000108,
 													password: 'root1234')
 
 nicolas = Volunteer.create(firstname: 'Nicolas',
@@ -62,14 +62,20 @@ croix_rouge = Assoc.create(name: 'Croix verte',
 													 description: 'Croix verte du swag',
 													 birthday: '02/12/2015',
 													 city: 'Paris',
-													 latitude: 9.99,
-													 longitude: 9.99)
+													 latitude: 48.874942, # Paris 10ème
+													 longitude: 2.358640)
 resto  = Assoc.create(name: "Les resto de l'estomac",
 											description: 'Pour se remplir le bide',
 											birthday: '02/12/2015',
-											city: 'Là-bas',
-											latitude: 9.99,
-											longitude: 9.99)
+											city: 'Paris',
+											latitude: 48.856662, # Paris 11ème
+											longitude: 2.377175)
+frere  = Assoc.create(name: "Les grands frères des pauvres",
+											description: 'Association personnes agées',
+											birthday: '02/12/1995',
+											city: 'Versailles',
+											latitude: 48.7949592, # Versailles
+											longitude: 2.081339)
 
 # AvLinks
 
@@ -84,6 +90,8 @@ membership resto, robin, 'member'
 membership resto, pierre, 'owner'
 membership resto, aude, 'admin'
 
+membership frere, aude, 'owner'
+
 # Events
 
 event_one = Event.create(title: 'Soignage de gens',
@@ -91,7 +99,7 @@ event_one = Event.create(title: 'Soignage de gens',
 												 place: 'Zimbabwe',
 												 begin: 2.days.from_now,
 												 end: 3.days.from_now,
-												 latitude: 48.85661400000001, # center paris
+												 latitude: 48.85661400000001, # Center Paris
 												 longitude: 2.3522219000000177,
 												 assoc_id: 1)
 
@@ -100,6 +108,8 @@ event_two = Event.create(title: 'Sauvetage du soldat Ryan',
 												 place: 'Normandie',
 												 begin: 2.days.from_now,
 												 end: 5.days.from_now,
+												 latitude: 49.870543, # Saint-Valery-en-Caux
+												 longitude: 0.692729,
 												 assoc_id: 1)
 
 event_three = Event.create(title: 'Donnage de miam miam',
@@ -107,6 +117,8 @@ event_three = Event.create(title: 'Donnage de miam miam',
 													 place: 'Paris',
 													 begin: 22.days.from_now,
 													 end: 23.days.from_now,
+												   latitude: 48.831373, # Place d'Italie
+												   longitude: 2.355711,
 													 assoc_id: 2)
 
 event_four = Event.create(title: 'Soirée Pizza !',
@@ -114,14 +126,27 @@ event_four = Event.create(title: 'Soirée Pizza !',
 													place: 'Italie',
 													begin: 256.days.from_now,
 													end: 258.days.from_now,
+												  latitude: 41.902130, # Rome
+												  longitude: 12.491259,
 													assoc_id: 2)
 
 event_five = Event.create(title: 'Buffet à volonté',
 													description: 'Sushi Maki Brochette',
-													place: 'Tokyo-Chine',
+													place: 'Tokyo',
 													begin: 2.days.from_now,
 													end: 10.days.from_now,
+ 												  latitude: 35.708990, # Tokyo
+ 												  longitude: 139.731307,
 													assoc_id: 2)
+
+event_frere = Event.create(title: 'Action grand frère',
+													 description: 'En cours',
+													 place: 'Versailles',
+													 begin: 2.days.from_now,
+													 end: 10.days.from_now,
+ 												   latitude: 48.7949592, # Versailles
+ 												   longitude: 2.081339,
+													 assoc_id: frere["id"])
 
 # EventVolunteers
 
@@ -136,6 +161,8 @@ guest event_two, jeremy, 'member'
 guest event_three, robin, 'host'
 guest event_four, pierre, 'host'
 guest event_five, aude, 'host'
+
+guest event_frere, aude, 'host'
 
 # Chatrooms
 
@@ -172,21 +199,26 @@ add_message chatroom_two, nicolas, "Cool"
 
 # Shelters
 
-Shelter.create([name: 'Un toit pour toi', address: 'Rue de Paris',
-							 zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150,
-							 description: "Dormir à l'abris toute l'année", assoc_id: 1])
-Shelter.create([name: 'Secours populaire', address: 'Rue voltaire',
-							 zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150,
-							 description: "", assoc_id: 1])
-Shelter.create([name: 'Auberge de la charité', address: 'Rue du chariton',
-							 zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150,
-							 description: "Ouvert à tous", assoc_id: 1])
-Shelter.create([name: 'Croix rouge', address: 'Rue du swag',
-							 zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150,
-							 description: "Nourriture et lit gratuit", assoc_id: 1])
-Shelter.create([name: 'Oyé logement', address: 'Rue du swag',
-							 zipcode: 75000, city: 'Paris', total_places: 200, free_places: 150,
-							 description: "", assoc_id: 2])
+Shelter.create([name: 'Un toit pour toi', address: '59 Rue de la Roquette',
+							 zipcode: 75011, city: 'Paris', total_places: 200, free_places: 150,
+							 description: "Dormir à l'abris toute l'année", assoc_id: 1,
+							 latitude: 48.8561268, longitude: 2.3750685])
+Shelter.create([name: 'Secours populaire', address: '75bis Boulevard de Clichy',
+							 zipcode: 75009, city: 'Paris', total_places: 200, free_places: 150,
+							 description: "", assoc_id: 1,
+							 latitude: 48.8841419, longitude: 2.3281687])
+Shelter.create([name: 'Auberge de la charité', address: '24 Boulevard de Grenelle',
+							 zipcode: 75015, city: 'Paris', total_places: 200, free_places: 150,
+							 description: "Ouvert à tous", assoc_id: 1,
+							 latitude: 48.85291, longitude: 2.287793])
+Shelter.create([name: 'Croix rouge', address: '47 Avenue Léon Gambetta',
+							 zipcode: 92120, city: 'Montrouge', total_places: 200, free_places: 150,
+							 description: "Nourriture et lit gratuit", assoc_id: 1,
+							 latitude: 48.8141777, longitude: 2.3188717])
+Shelter.create([name: 'Epitech Paris', address: '24 Rue Pasteur',
+							 zipcode: 94270, city: 'Le Kremlin-Bicêtre', total_places: 200, free_places: 150,
+							 description: "L'école de l'innovation!", assoc_id: 2,
+							 latitude: 48.8151016, longitude: 2.3569332])
 
 # Notifications
 
